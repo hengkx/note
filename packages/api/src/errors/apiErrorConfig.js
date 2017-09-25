45f62aa62cc231e26b18420a65826135ddadc5b6
hengkx <ycxzhkx@gmail.com>
2017-09-25 17:05:19 +0800

    change code

--- packages/api/src/errors/apiErrorConfig.js
+++ packages/api/src/errors/apiErrorConfig.js
@@ -1,8 +1,8 @@
 export default {
-  UNKNOW_ERROR: { code: -1, message: '未知错误' },
+  UNKNOW_ERROR: { code: 500, message: '系统错误' },
   UN_AUTHORIZE: { code: 401, message: '未授权' },
-  EMAIL_INVALID: { code: -1, message: '邮箱格式错误' },
-  ACCOUNT_NOT_ACTIVATED: { code: 1000, message: '帐号未激活，请先激活帐号在使用' },
-  USERNAME_OR_PASSWORD_ERROR: { code: 100, message: '用户名或密码不正确' },
-  USER_NOT_EXISTS: { code: 100, message: '用户不存在' },
+  EMAIL_INVALID: { code: 1000, message: '邮箱格式错误' },
+  ACCOUNT_NOT_ACTIVATED: { code: 2000, message: '帐号未激活，请先激活帐号在使用' },
+  USERNAME_OR_PASSWORD_ERROR: { code: 2001, message: '用户名或密码不正确' },
+  USER_NOT_EXISTS: { code: 2002, message: '用户不存在' },
 };
